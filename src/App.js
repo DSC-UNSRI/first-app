b568248b0302cbecaf3d1794ebbd3be049b07dd7
FadellMuhammad <fadel54031@gmail.com>
2021-06-06 19:29:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ini sudah diganti
+          sadasdasd
         </p>
         <a
           className="App-link"
