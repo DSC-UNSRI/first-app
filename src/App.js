11887bfc543c656e241942f5287601e68fc093bc
yogimahaputra <yogi.mahaputra123@gmail.com>
2021-06-06 19:29:58 +0700

    ini yogi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ini sudah diganti
+          Ini sudah digantiasdasdasasd
         </p>
         <a
           className="App-link"
